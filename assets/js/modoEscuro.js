62d354abc59486970711b8d7655d6a5aff013a4e
Naykyr Oliveira da Silva <103430603+NaykyrOliveira@users.noreply.github.com>
2022-11-20 09:26:52 -0300

    Ajustes no toggle de dark mode

--- assets/js/modoEscuro.js
+++ assets/js/modoEscuro.js
@@ -1,6 +1,7 @@
 const body = document.querySelector('body');
 const toggle = document.getElementById('toggle');
 const footer = document.querySelector('footer');
+
 toggle.onclick = function() {
     toggle.classList.toggle('active');
     body.classList.toggle('active');
